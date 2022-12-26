2dd1a7517d1cae2cacefe0bf5703e16a3f37eb2f
Rahul Ravindran <43089715+imrhlrvndrn@users.noreply.github.com>
2022-12-26 12:47:06 +0530

    Updated Cors

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ connectDb();
 configureCloudinary();
 
 // Express Middlewares
+app.set('Access-Control-Allow-Credentials', true);
+app.set('Access-Control-Allow-Headers', 'Content-Type');
+app.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+app.set('Access-Control-Allow-Origin', process.env.BACKEND_URL);
 app.use(
     cors({
         credentials: true,
